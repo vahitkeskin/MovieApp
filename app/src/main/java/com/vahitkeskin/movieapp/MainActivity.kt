c37ff6b344ff27ab6ec06ce0a555b418a3ffd18c
vahitkeskin <vhtkskn63@gmail.com>
2023-03-24 10:55:59 +0300

    StatusBar and navigationBar revision in design.

--- app/src/main/java/com/vahitkeskin/movieapp/MainActivity.kt
+++ app/src/main/java/com/vahitkeskin/movieapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.vahitkeskin.movieapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
